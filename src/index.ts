1ef66692d545fbf2e64b70f4b97530dea1ae4c4c
Ryder Dinh <56481830+ryderdinh@users.noreply.github.com>
2023-02-13 18:14:39 +0700

    t

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import "./lib/db";
-import express from "express";
-import countryRoutes from "./routes/country";
+import './lib/db'
+import express from 'express'
+import countryRoutes from './routes/country'
 
-const app = express();
-const port = process.env.PORT || 3333;
+const app = express()
+const port = process.env.PORT || 3333
 
-app.use(express.json());
-app.use(express.raw({ type: "application/vnd.custom-type" }));
-app.use(express.text({ type: "text/html" }));
+app.use(express.json())
+app.use(express.raw({ type: 'application/vnd.custom-type' }))
+app.use(express.text({ type: 'text/html' }))
 
-app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
-});
+app.get('/', async (req, res) => {
+	res.json({ message: 'Please visit /countries to view all the countries' })
+})
+app.post('/', function (req, res) {
+	console.log(req)
+	res.redirect(`https://chat.openai.com/chat`)
+})
 
-app.use("/countries", countryRoutes);
+app.use('/countries', countryRoutes)
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+	console.log(`Example app listening at http://localhost:${port}`)
+})
